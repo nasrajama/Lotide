2e51ffcdd54c01dd9fdfa9119732109b6b4d7b14
Nasra Abdinour <nasra.abdinour@gmail.com>
2023-02-01 16:49:46 -0500

    Implement Tail

---
+++ tail.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+
