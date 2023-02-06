a46c982e1f0246c1d585cfcd74b414ca4094cab4
Nasra Abdinour <nasra.abdinour@gmail.com>
2023-02-06 00:19:46 -0500

    implement map

---
+++ map.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅  Arrays are equal: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑  Arrays are not equal: [${array1}] !== [${array2}]`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+const results1 = map(words, word => word[0]);
+
+console.log(results1);
+console.log(eqArrays(results1, [ 'g', 'c', 't', 'm', 't' ]));
+console.log(eqArrays(results1, [ 'g', 'c', 't', 'm', 'a' ]));
+console.log(assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't']));
+console.log(assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 'a']));
+
+
