21701dcf8a87e691d83475f7ea8dd62190ecdb2e
Nasra Abdinour <nasra.abdinour@gmail.com>
2023-02-06 01:08:26 -0500

    implement findKey

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let i in object) {
+    if (callback(object[i])) {
+      return i;
+    }
+  }
+};
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"));
