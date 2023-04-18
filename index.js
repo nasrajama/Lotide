de643ba68758a5b0ac6472e6fb199b53c9be9949
Nasra Abdinour <nasra.abdinour@gmail.com>
2023-04-18 19:01:50 -0400

    added all functions required

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
 
 module.exports = {
   head,
@@ -12,4 +23,17 @@ module.exports = {
   eqArrays,
   assertEqual,
   assertArraysEqual,
+  assertObjectsEqual,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  flatten
 };
+
+
