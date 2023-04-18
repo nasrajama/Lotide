7706f162039d95e18ad22323d275cadbd2e9c6d3
Nasra Abdinour <nasra.abdinour@gmail.com>
2023-04-18 18:59:50 -0400

    export eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -46,3 +46,5 @@ assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), tr
 const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
 assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false)
+
+module.exports = eqObjects;
