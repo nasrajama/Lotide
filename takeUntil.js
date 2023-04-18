379898c02e2f588bdfb9edb2576ea2d1200cd0bc
Nasra Abdinour <nasra.abdinour@gmail.com>
2023-04-18 19:07:54 -0400

    removed unnecessary console.log & export takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -21,8 +21,8 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 
-const takeUntil = function(array, callback) {
-  const results = [];
+const takeUntil = (array, callback) => {
+  let results = [];
   for (let i of array) {
     if (!callback(i)) {
       results.push(i);
@@ -34,14 +34,11 @@ const takeUntil = function(array, callback) {
 }
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+const results1 = takeUntil(data1, (x) => x < 0);
 console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2 ]));
 
-
-console.log('---');
-
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+const results2 = takeUntil(data2, (x) => x === ',');
 console.log(assertArraysEqual(results2, ["I've", "been", "to", "Hollywood" ]));
+
+module.exports = takeUntil;
